7215ad2fecd8c27665ebcd566418c36ffcc0d5cc
zqm64 <zqm64@outlook.com>
2020-07-17 21:35:49 +0800

    ugglite: Update build fingerprint from latest MIUI Global release

--- lineage_ugglite.mk
+++ lineage_ugglite.mk
@@ -36,7 +36,7 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ugglite-user 7.1.2 N2G47H V10.2.1.0.NDKMIXM release-keys"
+    PRIVATE_BUILD_DESC="ugglite-user 7.1.2 N2G47H V11.0.3.0.NDFMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/ugglite/ugglite:7.1.2/N2G47H/V10.2.1.0.NDKMIXM:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/ugglite/ugglite:7.1.2/N2G47H/V11.0.3.0.NDFMIXM:user/release-keys"
